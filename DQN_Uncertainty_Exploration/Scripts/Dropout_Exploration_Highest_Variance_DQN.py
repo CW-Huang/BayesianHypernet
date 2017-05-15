a405a55354c7af60df97d5f1649909308a691426
Riashat <riashat.islam.93@gmail.com>
2017-05-15 00:11:19 -0400

    AL and RL Exps

---
+++ DQN_Uncertainty_Exploration/Scripts/Dropout_Exploration_Highest_Variance_DQN.py
@@ -0,0 +1,227 @@
+"""
+Pick actions with highest variance estimated from Q network 
+
+"""
+
+import copy
+import gym
+from gym import wrappers
+import matplotlib.pyplot as plt
+import time
+from utils import *
+from ReplayMemory import ReplayMemory
+from agents import AgentEpsGreedy
+from valuefunctions import ValueFunctionDQN
+from valuefunctions import ValueFunctionDQN3
+from valuefunctions import ValueFunctionDQN_TEST_TRAIN_DROPOUT
+from lib import plotting
+
+
+discount = 0.9
+decay_eps = 0.9
+batch_size = 64
+max_n_ep = 1000    #originally defined! Don't change this.
+
+min_avg_Rwd = 200000000  # Minimum average reward to consider the problem as solved
+n_avg_ep = 100      # Number of consecutive episodes to calculate the average reward
+
+
+def run_episode(env,
+                agent,
+                state_normalizer,
+                memory,
+                batch_size,
+                discount,
+                max_step=10000):
+    state = env.reset()
+    if state_normalizer is not None:
+        state = state_normalizer.transform(state)[0]
+    done = False
+    total_reward = 0
+    step_durations_s = np.zeros(shape=max_step, dtype=float)
+    train_duration_s = np.zeros(shape=max_step-batch_size, dtype=float)
+    progress_msg = "Step {:5d}/{:5d}. Avg step duration: {:3.1f} ms. Avg train duration: {:3.1f} ms. Loss = {:2.10f}."
+    loss_v = 0
+    w1_m = 0
+    w2_m = 0
+    w3_m = 0
+    i = 0
+    action = 0   
+
+    variance_steps = np.zeros(max_step)
+
+    #each step within an episode
+    for i in range(max_step):
+        t = time.time()
+        if i > 0 and i % 200 == 0:
+            print(progress_msg.format(i, max_step,
+                                      np.mean(step_durations_s[0:i])*1000,
+                                      np.mean(train_duration_s[0:i-batch_size])*1000,
+                                      loss_v))
+        if done:
+            break
+
+        """
+        Choose action that has the highest variance over the Q network
+        """
+        #actions based on maximising the q value
+        action, variance = agent.dropout_highest_variance(state)
+        variance_steps[i] = variance
+
+        #take a, get s' and reward
+        state_next, reward, done, info = env.step(action)
+        total_reward += reward
+
+        if state_normalizer is not None:
+            state_next = state_normalizer.transform(state_next)[0]
+
+
+        memory.add((state, action, reward, state_next, done))
+
+        if len(memory.memory) > batch_size:  # DQN Experience Replay
+            states_b, actions_b, rewards_b, states_n_b, done_b = zip(*memory.sample(batch_size))
+            states_b = np.array(states_b)
+            actions_b = np.array(actions_b)
+            rewards_b = np.array(rewards_b)
+            states_n_b = np.array(states_n_b)
+            done_b = np.array(done_b).astype(int)
+
+
+            '''
+            Dropout probability should be 1 here
+            '''
+            q_n_b = agent.predict_q_values(states_n_b)  # Action values on the arriving state
+
+            #target - Q-learning here - taking max_a over Q(s', a)
+            targets_b = rewards_b + (1. - done_b) * discount * np.amax(q_n_b, axis=1)
+
+            #target function for the agent - predict based on the trained Q Network
+            targets = agent.predict_q_values(states_b)
+            for j, action in enumerate(actions_b):
+                targets[j, action] = targets_b[j]
+
+            t_train = time.time()
+
+            #training the agent based on the target function
+            loss_v, w1_m, w2_m, w3_m = agent.train(states_b, targets)
+            train_duration_s[i - batch_size] = time.time() - t_train
+
+
+        state = copy.copy(state_next)
+        step_durations_s[i] = time.time() - t  # Time elapsed during this step
+        step_length = time.time() - t
+
+
+
+    return loss_v, w1_m, w2_m, w3_m, total_reward, step_length, variance_steps
+
+
+
+
+env = gym.make("CartPole-v0")
+
+n_actions = env.action_space.n
+
+state_dim = env.observation_space.high.shape[0]
+
+#max_step - number of steps within an episode
+max_n_ep = 2000      #number of episodes
+max_n_steps = 2000
+
+variance_episodes = np.zeros((max_n_steps, max_n_ep))
+
+Experiments = 1
+Experiments_All_Rewards = np.zeros(shape=(max_n_ep))
+
+
+
+
+for e in range(Experiments):
+
+
+    #using a smaller network for the value function
+    value_function = ValueFunctionDQN_TEST_TRAIN_DROPOUT(state_dim=state_dim, n_actions=n_actions, batch_size=batch_size)
+
+    #this is the temperature parameter in Boltzmann Distribution
+    tau_parameter = 1
+    agent = AgentEpsGreedy(n_actions=n_actions, value_function_model=value_function, eps=tau_parameter)
+    memory = ReplayMemory(max_size=100000)
+
+    print('Experiment Number ', e)
+
+    loss_per_ep = []
+    w1_m_per_ep = []
+    w2_m_per_ep = []
+    w3_m_per_ep = []
+    total_reward = []
+
+
+    ep = 0
+    avg_Rwd = -np.inf
+    episode_end_msg = 'loss={:2.10f}, w1_m={:3.1f}, w2_m={:3.1f}, w3_m={:3.1f}, total reward={}'
+
+    stats = plotting.EpisodeStats(episode_lengths=np.zeros(max_n_ep),episode_rewards=np.zeros(max_n_ep))  
+
+
+    """
+    Loop for the number of episodes : For every episode
+    """
+    while avg_Rwd < min_avg_Rwd and ep < max_n_ep:
+
+        
+        if ep >= n_avg_ep:
+            avg_Rwd = np.mean(total_reward[ep-n_avg_ep:ep])
+            print("EPISODE {}. Average reward over the last {} episodes: {}.".format(ep, n_avg_ep, avg_Rwd))
+        else:
+            print("EPISODE {}.".format(ep))
+
+
+        """
+        Contains loop for every step within the episode
+        """
+        loss_v, w1_m, w2_m, w3_m, cum_R, step_length, variance_steps = run_episode(env, agent, None, memory, batch_size=batch_size, discount=discount,
+                                                      max_step=max_n_steps)
+        
+        variance_episodes[:, ep] = variance_steps
+
+        print(episode_end_msg.format(loss_v, w1_m, w2_m, w3_m, cum_R))
+
+        stats.episode_rewards[ep] = cum_R
+        stats.episode_lengths[ep] = step_length
+
+
+        # Collect episode results
+        loss_per_ep.append(loss_v)
+        w1_m_per_ep.append(w1_m)
+        w2_m_per_ep.append(w2_m)
+        w3_m_per_ep.append(w3_m)
+        total_reward.append(cum_R)
+
+        ep += 1
+
+
+
+    Experiments_All_Rewards = Experiments_All_Rewards + total_reward
+    episode_length_over_time = stats.episode_lengths
+    variance_steps_episodes = variance_episodes
+
+
+    np.save('/Users/Riashat/Documents/PhD_Research/BASIC_ALGORITHMS/My_Implementations/Exploration_DQN/All_Results/'  + 'Cum_Rwd_' + 'Dropout_Highest_Variance_' + str(e) + '.npy', total_reward)
+    np.save('/Users/Riashat/Documents/PhD_Research/BASIC_ALGORITHMS/My_Implementations/Exploration_DQN/All_Results/'  + 'Variance_Values_' + 'Dropout_Highest_Variance_' + str(e) + '.npy', variance_steps_episodes)
+
+
+env.close()
+
+
+
+print('Saving Average Cumulative Rewards Over Experiments')
+
+Average_Cum_Rwd = np.divide(Experiments_All_Rewards, Experiments)
+
+np.save('/Users/Riashat/Documents/PhD_Research/BASIC_ALGORITHMS/My_Implementations/Exploration_DQN/All_Results/'   + 'Average_Cum_Rwd_' + 'Dropout_Highest_Variance_' + '.npy', Average_Cum_Rwd)
+
+
+print "All Experiments DONE - Deep Q Learning"
+
+
+print "Dropout with Highest Variance for Exploration DONE"
---
+++ DQN_Uncertainty_Exploration/valuefunctions.py
@@ -0,0 +1,331 @@
+import tensorflow as tf
+
+
+
+# 512 hidden units in layer 1, 256 units in layer2 - Larger Network
+class ValueFunctionDQN_TEST_TRAIN_DROPOUT:
+    def __init__(self, state_dim=2, n_actions=3, batch_size=64, train_size=44, valid_size=20):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            # Inputs, weights, biases and targets of the ANN
+            self.x = tf.placeholder(tf.float32, shape=(None, state_dim))                    # Single sample
+            self.train_data = tf.placeholder(tf.float32, shape=(batch_size, state_dim))     # Training batch of samples
+            self.eval_train_data = tf.placeholder(tf.float32, shape=(train_size, state_dim)) 
+            self.eval_valid_data = tf.placeholder(tf.float32, shape=(valid_size, state_dim))
+
+            self.train_targets = tf.placeholder(tf.float32, shape=(batch_size, n_actions))  # Training batch of targets
+            self.eval_train_targets = tf.placeholder(tf.float32, shape=(train_size, n_actions))
+            self.eval_valid_targets = tf.placeholder(tf.float32, shape=(valid_size, n_actions))
+
+            self.dropout_keep_prob = tf.placeholder(tf.float32)
+
+            #self.l1_weights = tf.Variable(tf.truncated_normal([state_dim, 512], stddev=0.1), trainable=True, name="w1")
+            self.l1_weights = tf.get_variable(name="w1", shape=[state_dim, 512],
+                                              initializer=tf.contrib.layers.xavier_initializer())
+            self.l1_biases = tf.Variable(tf.zeros([512]), trainable=True, name="b1")
+
+            #self.l2_weights = tf.Variable(tf.truncated_normal([512, 256], stddev=0.1), trainable=True, name="w2")
+            self.l2_weights = tf.get_variable(name="w2", shape=[512, 256],
+                                              initializer=tf.contrib.layers.xavier_initializer())
+            self.l2_biases = tf.Variable(tf.zeros([256]), trainable=True, name="b2")
+
+            #self.l3_weights = tf.Variable(tf.truncated_normal([256, n_actions], stddev=0.1), trainable=True, name="w3")
+            self.l3_weights = tf.get_variable(name="w3", shape=[256, n_actions],
+                                              initializer=tf.contrib.layers.xavier_initializer())
+            self.l3_biases = tf.Variable(tf.zeros([n_actions]), trainable=True, name="b3")
+
+            # Interconnection of the various ANN nodes
+            self.train_prediction = self.model(self.train_data)
+
+            self.eval_train_prediction = self.model_stochastic(self.eval_train_data, self.dropout_keep_prob)
+
+            self.eval_valid_prediction = self.model_stochastic(self.eval_valid_data, self.dropout_keep_prob)
+
+
+
+            # Training calculations - squared difference between the TD Target and TD Predictions
+            self.loss = tf.reduce_mean(tf.squared_difference(self.train_targets, self.train_prediction))
+
+            self.eval_train_loss = tf.reduce_mean(tf.squared_difference(self.eval_train_targets, self.eval_train_prediction))
+            self.eval_valid_loss = tf.reduce_mean(tf.squared_difference(self.eval_valid_targets, self.eval_valid_prediction))
+
+
+
+            #using Adam Optimizer for the training
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.loss)
+            self.eval_train_optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.eval_train_loss)
+            self.eval_valid_optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.eval_valid_loss)
+
+
+            self.test_prediction = self.model(self.x)
+
+            self.w1_max = tf.reduce_max(self.l1_weights)
+            self.w2_max = tf.reduce_max(self.l2_weights)
+            self.w3_max = tf.reduce_max(self.l3_weights)
+
+            self.init_op = tf.global_variables_initializer()
+
+        self.session = None
+
+
+    #DQN model is a two layer network
+    def model(self, data):
+        logits1 = tf.matmul(data, self.l1_weights) + self.l1_biases
+        hidden1 = tf.nn.relu(logits1)  # Define units of layer
+
+        logits2 = tf.matmul(hidden1, self.l2_weights) + self.l2_biases
+        hidden2 = tf.nn.relu(logits2)  # Define units of layer
+
+        out_layer = tf.matmul(hidden2, self.l3_weights) + self.l3_biases
+
+        return out_layer
+
+    #model with stochastic forward passes - used during batch train/valid split 
+    #only for applying MC Dropout
+    def model_stochastic(self, data, dropout_prob):
+        logits1 = tf.matmul(data, self.l1_weights) + self.l1_biases
+        hidden1 = tf.nn.relu(logits1)  # Define units of layer
+        drop_out = tf.nn.dropout(hidden1, dropout_prob)
+        logits2 = tf.matmul(hidden1, self.l2_weights) + self.l2_biases
+        hidden2 = tf.nn.relu(logits2)  # Define units of layer
+        drop_out2 = tf.nn.dropout(hidden2, dropout_prob)        
+        out_layer = tf.matmul(drop_out2, self.l3_weights) + self.l3_biases
+
+        return out_layer
+
+
+
+
+    def init_tf_session(self):
+        if self.session is None:
+            self.session = tf.Session(graph=self.graph)
+            self.session.run(self.init_op)  # Global Variables Initializer (init op)
+
+
+    def predict(self, states):
+        self.init_tf_session()  # Make sure the Tensorflow session exists
+
+        feed_dict = {self.x: states, self.dropout_keep_prob : 1.0}
+        q = self.session.run(self.test_prediction, feed_dict=feed_dict)
+        return q
+
+    # def predict_decaying(self, states, dropout_prob):
+    #     self.init_tf_session()  # Make sure the Tensorflow session exists
+
+    #     feed_dict = {self.x: states, self.dropout_keep_prob : dropout_prob}
+    #     q = self.session.run(self.test_prediction, feed_dict=feed_dict)
+
+    #     return q
+
+
+    def predict_stochastic(self, states):
+        self.init_tf_session()  # Make sure the Tensorflow session exists
+        feed_dict = {self.x: states, self.dropout_keep_prob : 0.1}
+        q = self.session.run(self.test_prediction, feed_dict=feed_dict)
+        return q
+
+
+
+    def train(self, states, targets):
+        self.init_tf_session()  # Make sure the Tensorflow session exists
+
+        #applying dropout to DQN during training
+        feed_dict = {self.train_data: states, self.train_targets: targets, self.dropout_keep_prob : 1.0}
+        [l, _, w1_m, w2_m, w3_m] = self.session.run([self.loss, self.optimizer, self.w1_max, self.w2_max, self.w3_max],
+                                                    feed_dict=feed_dict)
+
+        return [l, w1_m, w2_m, w3_m]
+
+
+    def eval_train(self, states, targets, dropout_probability):
+
+        self.init_tf_session()  # Make sure the Tensorflow session exists
+
+        #applying dropout to DQN during training
+        feed_dict = {self.eval_train_data: states, self.eval_train_targets: targets, self.dropout_keep_prob : dropout_probability}
+        [l, _, w1_m, w2_m, w3_m] = self.session.run([self.eval_train_loss, self.eval_train_optimizer, self.w1_max, self.w2_max, self.w3_max],
+                                                    feed_dict=feed_dict)
+        return [l, w1_m, w2_m, w3_m]
+
+
+
+    #WRONG HERE - this is training on the validation data
+    # def eval_valid(self, states, targets, dropout_probability):
+
+    #     self.init_tf_session()  # Make sure the Tensorflow session exists
+
+
+    #     #applying dropout to DQN during training
+    #     feed_dict = {self.eval_valid_data: states, self.eval_valid_targets: targets, self.dropout_keep_prob : dropout_probability}
+    #     [l, _, w1_m, w2_m, w3_m] = self.session.run([self.eval_valid_loss, self.eval_valid_optimizer, self.w1_max, self.w2_max, self.w3_max],
+    #                                                 feed_dict=feed_dict)
+    #     return [l, w1_m, w2_m, w3_m]
+
+
+    def eval_valid(self, states, targets, dropout_probability):
+
+        self.init_tf_session()  # Make sure the Tensorflow session exists
+
+
+        #applying dropout to DQN during training
+        feed_dict = {self.eval_valid_data: states, self.eval_valid_targets: targets, self.dropout_keep_prob : dropout_probability}
+        [l] = self.session.run([self.eval_valid_loss], feed_dict=feed_dict)
+        return [l]
+
+
+
+
+
+# 512 hidden units in layer 1, 256 units in layer2 - Larger Network
+class ValueFunctionDQN:
+    def __init__(self, state_dim=2, n_actions=3, batch_size=64):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            # Inputs, weights, biases and targets of the ANN
+            self.x = tf.placeholder(tf.float32, shape=(None, state_dim))                    # Single sample
+            self.train_data = tf.placeholder(tf.float32, shape=(batch_size, state_dim))     # Training batch of samples
+            self.train_targets = tf.placeholder(tf.float32, shape=(batch_size, n_actions))  # Training batch of targets
+
+            #self.l1_weights = tf.Variable(tf.truncated_normal([state_dim, 512], stddev=0.1), trainable=True, name="w1")
+            self.l1_weights = tf.get_variable(name="w1", shape=[state_dim, 512],
+                                              initializer=tf.contrib.layers.xavier_initializer())
+            self.l1_biases = tf.Variable(tf.zeros([512]), trainable=True, name="b1")
+
+            #self.l2_weights = tf.Variable(tf.truncated_normal([512, 256], stddev=0.1), trainable=True, name="w2")
+            self.l2_weights = tf.get_variable(name="w2", shape=[512, 256],
+                                              initializer=tf.contrib.layers.xavier_initializer())
+            self.l2_biases = tf.Variable(tf.zeros([256]), trainable=True, name="b2")
+
+            #self.l3_weights = tf.Variable(tf.truncated_normal([256, n_actions], stddev=0.1), trainable=True, name="w3")
+            self.l3_weights = tf.get_variable(name="w3", shape=[256, n_actions],
+                                              initializer=tf.contrib.layers.xavier_initializer())
+            self.l3_biases = tf.Variable(tf.zeros([n_actions]), trainable=True, name="b3")
+
+            # Interconnection of the various ANN nodes
+            self.train_prediction = self.model(self.train_data)
+
+            # Training calculations - squared difference between the TD Target and TD Predictions
+            self.loss = tf.reduce_mean(tf.squared_difference(self.train_targets, self.train_prediction))
+
+            #using Adam Optimizer for the training
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.loss)
+
+            self.test_prediction = self.model(self.x)
+
+            self.w1_max = tf.reduce_max(self.l1_weights)
+            self.w2_max = tf.reduce_max(self.l2_weights)
+            self.w3_max = tf.reduce_max(self.l3_weights)
+
+            self.init_op = tf.global_variables_initializer()
+
+        self.session = None
+
+    def model(self, data):
+        logits1 = tf.matmul(data, self.l1_weights) + self.l1_biases
+        hidden1 = tf.nn.relu(logits1)  # Define units of layer
+
+        logits2 = tf.matmul(hidden1, self.l2_weights) + self.l2_biases
+        hidden2 = tf.nn.relu(logits2)  # Define units of layer
+
+        return tf.matmul(hidden2, self.l3_weights) + self.l3_biases
+
+    def init_tf_session(self):
+        if self.session is None:
+            self.session = tf.Session(graph=self.graph)
+            self.session.run(self.init_op)  # Global Variables Initializer (init op)
+
+    def predict(self, states):
+        self.init_tf_session()  # Make sure the Tensorflow session exists
+
+        feed_dict = {self.x: states}
+        q = self.session.run(self.test_prediction, feed_dict=feed_dict)
+        return q
+
+    def train(self, states, targets):
+        self.init_tf_session()  # Make sure the Tensorflow session exists
+
+        feed_dict = {self.train_data: states, self.train_targets: targets}
+        [l, _, w1_m, w2_m, w3_m] = self.session.run([self.loss, self.optimizer, self.w1_max, self.w2_max, self.w3_max],
+                                                    feed_dict=feed_dict)
+
+        return [l, w1_m, w2_m, w3_m]
+
+    
+
+
+
+
+# # Smaller network: 128 neurons on layer 1, 64 neurons on layer 2
+# class ValueFunctionDQN3:
+
+#     def __init__(self, state_dim=2, n_actions=3, batch_size=64):
+#         self.graph = tf.Graph()
+#         with self.graph.as_default():
+#             # Inputs, weights, biases and targets of the ANN
+#             self.x = tf.placeholder(tf.float32, shape=(None, state_dim))                    # Single sample
+#             self.train_data = tf.placeholder(tf.float32, shape=(batch_size, state_dim))     # Training batch of samples
+#             self.train_targets = tf.placeholder(tf.float32, shape=(batch_size, n_actions))  # Training batch of targets
+
+#             self.l1_weights = tf.get_variable(name="w1", shape=[state_dim, 128],
+#                                               initializer=tf.contrib.layers.xavier_initializer())
+#             self.l1_biases = tf.Variable(tf.zeros([128]), trainable=True, name="b1")
+
+#             self.l2_weights = tf.get_variable(name="w2", shape=[128, 64],
+#                                               initializer=tf.contrib.layers.xavier_initializer())
+#             self.l2_biases = tf.Variable(tf.zeros([64]), trainable=True, name="b2")
+
+#             self.l3_weights = tf.get_variable(name="w3", shape=[64, n_actions],
+#                                               initializer=tf.contrib.layers.xavier_initializer())
+#             self.l3_biases = tf.Variable(tf.zeros([n_actions]), trainable=True, name="b3")
+
+#             # Interconnection of the various ANN nodes
+#             self.train_prediction = self.model(self.train_data)
+
+#             # Training calculations
+#             self.loss = tf.reduce_mean(tf.squared_difference(self.train_targets, self.train_prediction))
+#             self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.loss)
+
+#             self.test_prediction = self.model(self.x)
+
+#             self.w1_max = tf.reduce_max(self.l1_weights)
+#             self.w2_max = tf.reduce_max(self.l2_weights)
+#             self.w3_max = tf.reduce_max(self.l3_weights)
+
+#             self.init_op = tf.global_variables_initializer()
+
+#         self.session = None
+
+#     def model(self, data):
+#         logits1 = tf.matmul(data, self.l1_weights) + self.l1_biases
+#         hidden1 = tf.nn.relu(logits1)  # Define units of layer
+
+#         logits2 = tf.matmul(hidden1, self.l2_weights) + self.l2_biases
+#         hidden2 = tf.nn.relu(logits2)  # Define units of layer
+
+#         return tf.matmul(hidden2, self.l3_weights) + self.l3_biases
+
+#     def init_tf_session(self):
+#         if self.session is None:
+#             self.session = tf.Session(graph=self.graph)
+#             self.session.run(self.init_op)  # Global Variables Initializer (init op)
+
+#     def predict(self, states):
+#         self.init_tf_session()  # Make sure the Tensorflow session exists
+
+#         feed_dict = {self.x: states}
+#         q = self.session.run(self.test_prediction, feed_dict=feed_dict)
+#         return q
+
+#     def train(self, states, targets):
+#         self.init_tf_session()  # Make sure the Tensorflow session exists
+
+#         feed_dict = {self.train_data: states, self.train_targets: targets}
+#         [l, _, w1_m, w2_m, w3_m] = self.session.run([self.loss, self.optimizer, self.w1_max, self.w2_max, self.w3_max],
+#                                                     feed_dict=feed_dict)
+
+#         return [l, w1_m, w2_m, w3_m]
+
+
+
+
+
