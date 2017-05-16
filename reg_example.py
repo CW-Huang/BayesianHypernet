95001acad3687a012df64761decf5e2dd50968d7
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-05-16 12:15:23 -0400

    optimization seems more robust for fully factored posterior approx.

--- reg_example.py
+++ reg_example.py
@@ -36,6 +36,7 @@ def primary_net_f(X, theta, weight_shapes):
     yp = T.dot(a1, W1) + b1[None, :]
 
     y_prec = T.exp(log_prec)
+    #y_prec = T.constant(1.0)
     return yp, y_prec
 
 
@@ -43,20 +44,24 @@ def loglik_primary_f(X, y, theta, weight_shapes):
     yp, y_prec = primary_net_f(X, theta, weight_shapes)
     err = yp - y
 
-    # Ignoring normalizing constant
-    loglik = -0.5 * y_prec * T.sum(err ** 2, axis=1)
+    loglik_c = -0.5 * np.log(2.0 * np.pi)
+    loglik_cmp = 0.5 * T.log(y_prec)  # Hopefully Theano undoes log(exp())
+    loglik_fit = -0.5 * y_prec * T.sum(err ** 2, axis=1)
+    loglik = loglik_c + loglik_cmp + loglik_fit
     return loglik
 
 
 def logprior_f(theta):
     # Standard Gauss
     logprior = -0.5 * T.sum(theta ** 2)  # Ignoring normalizing constant
+    #logprior = T.constant(0.0)  # TODO undo
     return logprior
 
 
 def simple_test(X, y, X_valid, y_valid,
                 n_epochs, n_batch, init_lr, weight_shapes,
-                n_layers=5, vis_freq=100, n_samples=100):
+                n_layers=5, vis_freq=100, n_samples=100,
+                z_std=1.0):
     N, D = X.shape
     N_valid = X_valid.shape[0]
     assert(y.shape == (N, 1))  # Univariate for now
@@ -64,8 +69,15 @@ def simple_test(X, y, X_valid, y_valid,
 
     num_params = sum(np.prod(ws, dtype=int) for ws in weight_shapes)
 
-    layers = ign.init_ign(n_layers, num_params)
-    phi_shared = make_shared_dict(layers, '%d%s')
+    #layers = ign.init_ign(n_layers, num_params, rnd_W=False)
+
+    layers = {}
+    layers[(0, 'WL')] = 1e-2 * np.ones(num_params)
+    layers[(0, 'bL')] = np.random.randn(num_params)
+    layers[(0, 'bL')][-1] = 6.0  # Start with big precision
+    phi_shared_real = make_shared_dict(layers, '%d%s')
+    phi_shared = phi_shared_real.copy()
+    phi_shared[(0, 'WL')] = T.nlinalg.alloc_diag(phi_shared_real[(0, 'WL')])
 
     ll_primary_f = lambda X, y, w: loglik_primary_f(X, y, w, weight_shapes)
     hypernet_f = lambda z: ign.network_T_and_J(z[None, :], phi_shared)[0][0, :]
@@ -73,7 +85,14 @@ def simple_test(X, y, X_valid, y_valid,
     log_det_dtheta_dz_f = lambda z: T.sum(ign.network_T_and_J(z[None, :], phi_shared)[1])
     primary_f = lambda X, w: primary_net_f(X, w, weight_shapes)
 
-    R = ht.build_trainer(phi_shared.values(), N, ll_primary_f, logprior_f,
+    params_to_opt = [phi_shared_real[(0, 'bL')]]  # TODO remove
+    R = ht.build_trainer(params_to_opt, N, ll_primary_f, logprior_f,
+                         hypernet_f, primary_f=primary_f,
+                         log_det_dtheta_dz_f=log_det_dtheta_dz_f)
+    trainer_prelim = R[0]
+
+    params_to_opt = phi_shared_real.values()
+    R = ht.build_trainer(params_to_opt, N, ll_primary_f, logprior_f,
                          hypernet_f, primary_f=primary_f,
                          log_det_dtheta_dz_f=log_det_dtheta_dz_f)
     trainer, get_err, test_loglik, primary_out = R
@@ -86,12 +105,16 @@ def simple_test(X, y, X_valid, y_valid,
         np.random.shuffle(batch_order)
 
         cost = 0.0
-        current_lr = init_lr
         for ii in batch_order:
             x_batch = X[ii * n_batch:(ii + 1) * n_batch]
             y_batch = y[ii * n_batch:(ii + 1) * n_batch]
-            z_noise = np.random.randn(num_params)
-            batch_cost = trainer(x_batch, y_batch, z_noise, current_lr)
+            z_noise = z_std * np.random.randn(num_params)
+            if epoch <= 10:
+                current_lr = init_lr
+                batch_cost = trainer_prelim(x_batch, y_batch, z_noise, current_lr)
+            else:
+                current_lr = init_lr * 0.1
+                batch_cost = trainer(x_batch, y_batch, z_noise, current_lr)
             cost += batch_cost
         cost /= len(batch_order)
         print cost
@@ -99,13 +122,13 @@ def simple_test(X, y, X_valid, y_valid,
 
         loglik_valid_s = np.zeros((N_valid, n_samples))
         for ss in xrange(n_samples):
-            z_noise = np.random.randn(num_params)
+            z_noise = z_std * np.random.randn(num_params)
             loglik_valid_s[:, ss] = test_loglik(X_valid, y_valid, z_noise)
         loglik_valid_s_adj = loglik_valid_s - np.log(n_samples)
         loglik_valid[epoch] = np.mean(logsumexp(loglik_valid_s_adj, axis=1))
         print 'valid %f' % loglik_valid[epoch]
 
-    phi = make_unshared_dict(phi_shared)
+    phi = make_unshared_dict(phi_shared_real)
     return phi, cost_hist, loglik_valid, primary_out
 
 
@@ -115,9 +138,10 @@ if __name__ == '__main__':
     np.random.seed(5645)
 
     init_lr = 0.001
-    n_epochs = 100
+    n_epochs = 500
     n_batch = 32
     N = 1000
+    z_std = 1.0  # 1.0 is correct for the model, 0.0 is MAP
 
     primary_layers = 1
     input_dim = 1
@@ -132,7 +156,7 @@ if __name__ == '__main__':
     phi, cost_hist, loglik_valid, primary_out = \
         simple_test(X, y, X_valid, y_valid,
                     n_epochs, n_batch, init_lr, weight_shapes,
-                    n_layers=2)
+                    n_layers=1, z_std=z_std)
 
     n_samples = 500
     n_grid = 1000
@@ -142,7 +166,7 @@ if __name__ == '__main__':
     mu_grid = np.zeros((n_samples, n_grid))
     y_grid = np.zeros((n_samples, n_grid))
     for ss in xrange(n_samples):
-        z_noise = np.random.randn(num_params)
+        z_noise = z_std * np.random.randn(num_params)
         mu, prec = primary_out(x_grid[:, None], z_noise)
         std_dev = np.sqrt(1.0 / prec)
         # Just store an MC version than do the mixture of Gauss logic
