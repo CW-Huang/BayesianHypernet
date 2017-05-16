c85bb49d07a404da5f2429853bb5f6fa3328a3b3
CW-Huang <cw.huang427@gmail.com>
2017-05-16 16:23:46 -0400

    fix a bug

--- experiment_MLP_WN.py
+++ experiment_MLP_WN.py
@@ -111,10 +111,9 @@ def train_model(train_func,predict_func,X,Y,Xt,Yt,
     return records
 
 
-def evaluate_model(predict_proba,X,Y,Xt,Yt,n_mc=1000):
-    n = X.shape[0]
-    MCt = np.zeros((n_mc,n,10))
-    MCv = np.zeros((n_mc,n,10))
+def evaluate_model(predict_proba,X,Y,Xt,Yt,n_mc=100):
+    MCt = np.zeros((n_mc,X.shape[0],10))
+    MCv = np.zeros((n_mc,Xt.shape[0],10))
     for i in range(n_mc):
         MCt[i] = predict_proba(X)
         MCv[i] = predict_proba(Xt)
@@ -134,11 +133,11 @@ def evaluate_model(predict_proba,X,Y,Xt,Yt,n_mc=1000):
     
     ind = ind_negative[-1] #TO-DO: complete evaluation
     for ii in range(15): 
-        print np.round(MCt[ii][ind] * 1000)
+        print np.round(MCv[ii][ind] * 1000)
     
     ind = ind_negative[-2] #TO-DO: complete evaluation
     for ii in range(15): 
-        print np.round(MCt[ii][ind] * 1000)
+        print np.round(MCv[ii][ind] * 1000)
 
 #def main():
 if __name__ == '__main__':
