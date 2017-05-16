45187f361153e21692c9d687a9e238193093d30d
CW-Huang <cw.huang427@gmail.com>
2017-05-16 00:42:52 -0400

    dropout for MLP

--- experiment_MLP_WN.py
+++ experiment_MLP_WN.py
@@ -10,6 +10,70 @@ from ops import load_mnist
 from utils import log_normal, log_laplace
 import numpy as np
 
+import lasagne
+import theano
+import theano.tensor as T
+
+
+class MLPWeightNorm_BHN_full(MLPWeightNorm_BHN):
+    
+    weight_shapes = [(784, 800),
+                     (800, 800),
+                     (800,  10)]
+    num_params = sum(ws[1] for ws in weight_shapes)
+    
+
+    
+    
+class MCdropout_MLP(object):
+    
+    weight_shapes = [(784, 800),
+                     (800, 800),
+                     (800,  10)]
+                     
+    def __init__(self):
+        
+        layer = lasagne.layers.InputLayer([None,784])
+        
+        for j,ws in enumerate(self.weight_shapes):
+            layer = lasagne.layers.DenseLayer(
+                layer,ws[1],
+                nonlinearity=lasagne.nonlinearities.rectify
+            )
+            if j!=len(self.weight_shapes)-1:
+                layer = lasagne.layers.dropout(layer)
+        
+        layer.nonlinearity = lasagne.nonlinearities.softmax
+        self.input_var = T.matrix('input_var')
+        self.target_var = T.matrix('target_var')
+        self.learning_rate = T.scalar('leanring_rate')
+        self.dataset_size = T.scalar('dataset_size') # useless
+        
+        self.layer = layer
+        self.y = lasagne.layers.get_output(layer,self.input_var)
+        self.y_det = lasagne.layers.get_output(layer,self.input_var,
+                                               deterministic=True)
+        
+        losses = lasagne.objectives.categorical_crossentropy(self.y,
+                                                             self.target_var)
+        self.loss = losses.mean() + self.dataset_size * 0.
+        self.params = lasagne.layers.get_all_params(self.layer)
+        self.updates = lasagne.updates.adam(self.loss,self.params,
+                                            self.learning_rate)
+
+        print '\tgetting train_func'
+        self.train_func = theano.function([self.input_var,
+                                           self.target_var,
+                                           self.dataset_size,
+                                           self.learning_rate],
+                                          self.loss,
+                                          updates=self.updates)
+        
+        print '\tgetting useful_funcs'
+        self.predict_proba = theano.function([self.input_var],self.y)
+        self.predict = theano.function([self.input_var],self.y_det.argmax(1))
+        
+
 
 
 
@@ -68,10 +132,13 @@ def evaluate_model(predict_proba,X,Y,Xt,Yt,n_mc=1000):
     ind_positive = np.arange(Xt.shape[0])[Yt_pred == Yt_true]
     ind_negative = np.arange(Xt.shape[0])[Yt_pred != Yt_true]
     
-    ind = ind_negative[0] #TO-DO: complete evaluation
+    ind = ind_negative[-1] #TO-DO: complete evaluation
     for ii in range(15): 
         print np.round(MCt[ii][ind] * 1000)
-        
+    
+    ind = ind_negative[-2] #TO-DO: complete evaluation
+    for ii in range(15): 
+        print np.round(MCt[ii][ind] * 1000)
 
 #def main():
 if __name__ == '__main__':
@@ -91,6 +158,8 @@ if __name__ == '__main__':
     parser.add_argument('--bs',default=20,type=int)  
     parser.add_argument('--epochs',default=50,type=int)
     parser.add_argument('--prior',default='log_normal',type=str)
+    parser.add_argument('--model',default='BHN_MLPWN',type=str)
+    
     args = parser.parse_args()
     print args
     
@@ -105,16 +174,23 @@ if __name__ == '__main__':
         prior = log_normal
     elif args.prior=='log_laplace':
         prior = log_laplace
+    else:
+        raise Exception('no prior named `{}`'.format(args.prior))
     size = max(10,min(50000,args.size))
     
     filename = '/data/lisa/data/mnist.pkl.gz'
     train_x, train_y, valid_x, valid_y, test_x, test_y = load_mnist(filename)
-    model = MLPWeightNorm_BHN(lbda=lbda,
-                              perdatapoint=perdatapoint,
-                              prior=prior,
-                              coupling=coupling)
     
-    
+    if args.model == 'BHN_MLPWN':
+        model = MLPWeightNorm_BHN_full(lbda=lbda,
+                                       perdatapoint=perdatapoint,
+                                       prior=prior,
+                                       coupling=coupling)
+    elif args.model == 'MCdropout_MLP':
+        model = MCdropout_MLP()
+    else:
+        raise Exception('no model named `{}`'.format(args.model))
+        
     recs = train_model(model.train_func,model.predict,
                        train_x[:size],train_y[:size],
                        valid_x,valid_y,
@@ -123,4 +199,10 @@ if __name__ == '__main__':
     evaluate_model(model.predict_proba,
                    train_x[:size],train_y[:size],
                    valid_x,valid_y)
+    
+    evaluate_model(model.predict_proba,
+                   train_x[:size],train_y[:size],
+                   test_x,test_y)
+
+
 
