2c88e096fd5e5dff1733b3fb10896cfe72850978
David Krueger <davidscottkrueger@gmail.com>
2017-10-13 15:49:05 -0400

    delete files moved to OLD_Oct12

--- OLD_Oct12/Active_Learning_Tasks/MC_Dropout_Keras/keras/build/lib/keras/layers/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from __future__ import absolute_import
-from .core import *
-from .convolutional import *
-from .recurrent import *
-from .normalization import *
-from .embeddings import *
-from .noise import *
-from .advanced_activations import *
