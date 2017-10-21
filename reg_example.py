4d08dd7ce3ca9a4eef30b42bde318926dbd6b38d
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-10-21 12:59:12 -0400

    cleaning up mlp_hmc to make more general

--- reg_example.py
+++ reg_example.py
@@ -44,7 +44,7 @@ def simple_test(X, y, X_valid, y_valid,
     layers0 = ign.init_ign_LU(n_layers, num_params, WL_val=WL_init)
     phi_shared = make_shared_dict(layers0, '%d%s')
 
-    # TODO use innfer functions to make this simpler
+    # TODO use inner functions to make this simpler
 
     ll_primary_f = lambda X, y, w: mlp_hmc.mlp_loglik_flat_tt(X, y, w, weight_shapes)
     hypernet_f = lambda z, prelim=False: ign.network_T_and_J_LU(z[None, :], phi_shared, force_diag=prelim)[0][0, :]
@@ -90,7 +90,7 @@ def simple_test(X, y, X_valid, y_valid,
             loglik_valid_s[:, ss] = test_loglik(X_valid, y_valid, z_noise, False)
         loglik_valid_s_adj = loglik_valid_s - np.log(n_samples)
         loglik_valid[epoch] = np.mean(logsumexp(loglik_valid_s_adj, axis=1))
-        print 'valid %f' % loglik_valid[epoch]
+        print 'valid %d %f' % (epoch, loglik_valid[epoch])
 
     phi = make_unshared_dict(phi_shared)
     return phi, cost_hist, loglik_valid, primary_out, grad_f, theta_f
@@ -140,18 +140,19 @@ def traditional_test(X, y, X_valid, y_valid, n_epochs, n_batch, init_lr, weight_
         print cost
         cost_hist[epoch] = cost
         loglik_valid[epoch] = np.mean(test_loglik(X, y))
-        print 'valid %f' % loglik_valid[epoch]
+        print 'valid %d %f' % (epoch, loglik_valid[epoch])
 
     phi = make_unshared_dict(phi_shared)
     return phi, cost_hist, loglik_valid, primary_out
 
 if __name__ == '__main__':
     np.random.seed(5645)
+    test_run = False
 
-    n_tune_hmc = 50
-    n_iter_hmc = 50
-    n_samples = 100
-    n_epochs = 1000
+    n_tune_hmc = 5 if test_run else 50
+    n_iter_hmc = 3 if test_run else 50
+    n_samples = 5 if test_run else 100
+    n_epochs = 3 if test_run else 1000
 
     init_lr = 0.0005
     n_batch = 32
@@ -159,8 +160,7 @@ if __name__ == '__main__':
     z_std = 1.0  # 1.0 is correct for the model, 0.0 is MAP
     n_grid = 1000
 
-    # TODO put summary quantiles here
-
+    # Primary network
     input_dim = 1
     hidden_dim = 50
     output_dim = 1
@@ -170,28 +170,30 @@ if __name__ == '__main__':
                                  ('W_1', (hidden_dim, output_dim)),
                                  ('b_1', (output_dim,)),
                                  ('log_prec', ())])
-    n_layers, rem = divmod(len(weight_shapes) - 1, 2)
-    assert(rem == 0)
 
     X, y = dm_example(N)
     X_valid, y_valid = dm_example(N)
 
     x_grid = np.linspace(-0.5, 1.5, n_grid)
 
+    print 'hypernet training'
     phi, cost_hist, loglik_valid, primary_out, grad_f, hypernet_f = \
         simple_test(X, y, X_valid, y_valid,
                     n_epochs, n_batch, init_lr, weight_shapes,
                     n_layers=3, n_samples=n_samples, z_std=z_std)
 
+    print 'traditional training'
     phi_trad, cost_hist_trad, loglik_valid_trad, primary_out_trad = \
         traditional_test(X, y, X_valid, y_valid, n_epochs, n_batch, init_lr, weight_shapes)
 
-    tr, hmc_dbg = mlp_hmc.hmc_net(X, y, X_valid, y_valid, hypernet_f, weight_shapes, restarts=n_samples, n_iter=n_iter_hmc, n_tune=n_tune_hmc)
+    tr, hmc_dbg = mlp_hmc.hmc_net(X, y, X_valid, y_valid, hypernet_f,
+                                  weight_shapes, restarts=n_samples,
+                                  n_iter=n_iter_hmc, n_tune=n_tune_hmc)
 
     mu_hmc, std_hmc, LB_hmc, UB_hmc, _, _ = \
-        mlp_hmc.hmc_pred(tr, x_grid[:, None], n_layers=n_layers)
+        mlp_hmc.hmc_pred(tr, x_grid[:, None])
     _, _, _, _, loglik_hmc, loglik_raw = \
-        mlp_hmc.hmc_pred(tr, X_valid, y_test=y_valid[:, 0], n_layers=n_layers)
+        mlp_hmc.hmc_pred(tr, X_valid, y_test=y_valid[:, 0])
 
     # Debug check to make sure get same answer for loglik
     _, _, loglik_test_dbg = hmc_dbg
