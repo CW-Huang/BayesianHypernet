587d1f0529e87d1a2f23855dabbc2d38b271dcbf
Riashat Islam <riashat.islam.93@gmail.com>
2017-10-24 23:23:16 -0400

    chain environment setup (TODO)

--- RL_Experiments/experiment_RL_Chain.py
+++ RL_Experiments/experiment_RL_Chain.py
@@ -1,102 +1,130 @@
-
-import json
-import numpy as np
-
-#from keras.models_trial import Sequential
-from keras.models import Sequential
-from keras.layers.core import Dense
-from keras.optimizers import sgd
-from keras import backend as K
-from keras.layers.core import Lambda
-
 from dropout import MCdropout_MLP
+from BHNs import MLPWeightNorm_BHN
+from theano.tensor.shared_randomstreams import RandomStreams
+import argparse
+import os
+import sys
+import numpy 
+np = numpy
+seed = np.random.randint(2**32-1)
 
-#import theano
-#theano.config.floatX='float32'
-
-# model.predict
-# model.train_on_batch
-
-class Catch(object):
-    def __init__(self, grid_size=10):
-        self.grid_size = grid_size
-        self.reset()
-
-    def _update_state(self, action):
-        """
-        Input: action and states
-        Ouput: new states and reward
-        """
-        state = self.state
-        if action == 0:  # left
-            action = -1
-        elif action == 1:  # stay
-            action = 0
-        else:
-            action = 1  # right
-        f0, f1, basket = state[0]
-        new_basket = min(max(1, basket + action), self.grid_size-1)
-        f0 += 1
-        out = np.asarray([f0, f1, new_basket])
-        out = out[np.newaxis]
-
-        assert len(out.shape) == 2
-        self.state = out
-
-    def _draw_state(self):
-        im_size = (self.grid_size,)*2
-        state = self.state[0]
-        canvas = np.zeros(im_size)
-        canvas[state[0], state[1]] = 1  # draw fruit
-        canvas[-1, state[2]-1:state[2] + 2] = 1  # draw basket
-        return canvas
-
-    def _get_reward(self):
-        fruit_row, fruit_col, basket = self.state[0]
-        if fruit_row == self.grid_size-1:
-            if abs(fruit_col - basket) <= 1:
-                return 1
-            else:
-                return -1
-        else:
-            return 0
-
-    def _is_over(self):
-        if self.state[0, 0] == self.grid_size-1:
-            return True
-        else:
-            return False
-
-    def observe(self):
-        canvas = self._draw_state()
-        return canvas.reshape((1, -1))
-
-    def act(self, action):
-        self._update_state(action)
-        reward = self._get_reward()
-        game_over = self._is_over()
-        return self.observe(), reward, game_over
+parser = argparse.ArgumentParser()
+parser.add_argument('--drop_prob', type=float, default=.0)
+#parser.add_argument('--bs', type=int, default=50)
+parser.add_argument('--exploration', type=str, default='epsilon_greedy', choices=['epsilon_greedy', 'RLSVI', 'TS'])
+parser.add_argument('--lr', type=float, default=.2)
+parser.add_argument('--model', type=str, default='BHN', choices=['BHN', 'MCD'])
+#parser.add_argument('--n_epochs', type=int, default=1000)
+#parser.add_argument('--n_layers', type=int, default=2)
+#parser.add_argument('--n_hids', type=int, default=100)
+#
+#parser.add_argument('--opt', type=str, default='momentum', choices=['adam', 'momentum', 'sgd'])
+#parser.add_argument('--save_dir', type=str, default=None, help="save_dir must be set in order to save results!")
+#parser.add_argument('--seed', type=int, default=1337)
+#parser.add_argument('--verbose', type=int, default=1)
+locals().update(parser.parse_args().__dict__)
 
 
-    def mc_dropout_act(self, model, input_tm):
-        mc_samples = 50
-        q_value = model.predict(input_tm)
-        all_q_values = np.zeros(shape=(mc_samples, q_value.shape[1]))
 
-        for m in range(mc_samples):
-            q_value = model.predict(input_tm)
-            all_q_values[m, :] = q_value
+class Chain(object):
+    def __init__(self, nState, nAction, epLen): 
+        '''
+        Initialize a tabular episodic MDP
 
-        mean_q_values = np.array([np.mean(all_q_values, axis=0)])
-        action = np.argmax(mean_q_values[0])
+        Args:
+            nState  - int - number of states
+            nAction - int - number of actions
+            epLen   - int - episode length
 
-        return action
+        Returns:
+            Environment object
+        '''
+
+        self.nState = nState
+        self.nAction = nAction
+        self.epLen = epLen
+
+        self.timestep = 0
+        self.state = 0
+
+        # Now initialize R and P
+        self.R = {}
+        self.P = {}
+        for state in range(nState):
+            for action in range(nAction):
+                self.R[state, action] = (1, 1)
+                self.P[state, action] = np.ones(nState) / nState
 
 
     def reset(self):
-        n = np.random.randint(0, self.grid_size-1, size=1)
-        m = np.random.randint(1, self.grid_size-2, size=1)
-        self.state = np.asarray([0, n, m])[np.newaxis]
+        '''Reset the environment'''
+        self.timestep = 0
+        self.state = 0
+          
+
+    def act(self, action):
+        if self.R[self.state, action][1] < 1e-9:
+            # Hack for no noise
+            reward = self.R[self.state, action][0]
+        else:
+            reward = np.random.normal(loc=self.R[self.state, action][0],
+                                      scale=self.R[self.state, action][1])
+        newState = np.random.choice(self.nState, p=self.P[self.state, action])
+
+        # Update the environment
+        self.state = newState
+        self.timestep += 1
+
+        if self.timestep == self.epLen:
+            pContinue = 0
+            self.reset()
+        else:
+            pContinue = 1
+
+        return reward, newState, pContinue
+
+
+
+    def observe(self):
+        self.state = np.array([0])
+        return self.state
+
+
+def make_bootDQNChain(nState=6, epLen=15, nAction=2):
+    '''
+    Creates the chain from Bootstrapped DQN
+
+    Returns:
+        bootDQNChain - Tabular MDP environment
+    '''
+    R_true = {}
+    P_true = {}
+
+    for s in xrange(nState):
+        for a in xrange(nAction):
+            R_true[s, a] = (0, 0)
+            P_true[s, a] = np.zeros(nState)
+
+    # Rewards
+    R_true[0, 0] = (0.01, 1)
+    R_true[nState - 1, 1] = (1, 1)
+
+    # Transitions
+    for s in xrange(nState):
+        P_true[s, 0][max(0, s-1)] = 1.
+
+        P_true[s, 1][min(nState - 1, s + 1)] = 0.5
+        P_true[s, 1][max(0, s-1)] = 0.5
+
+    bootDQNChain = Chain(nState, nAction, epLen)
+    bootDQNChain.R = R_true
+    bootDQNChain.P = P_true
+    bootDQNChain.reset()
+
+    return bootDQNChain
+
+
+
 
 
 class ExperienceReplay(object):
@@ -144,7 +172,11 @@ if __name__ == "__main__":
     hidden_size = 100
     batch_size = 50
     grid_size = 10
-    dropout=0.
+    dropout=0.1
+
+    exploration = 'RLSVI'
+    #exploration = 'TS'
+    #exploration = 'epsilon_greedy'
 
     if 0:
         model = Sequential()
@@ -158,10 +190,7 @@ if __name__ == "__main__":
         if dropout: # TODO: rm this! (no dropout at the output, brah!
             model.add(Lambda(lambda x: K.dropout(x, level=0.5)))
         model.compile(sgd(lr=.2), "mse")
-    else:
-
-        print ("MC Droput MLP")
-
+    elif model == 'MCD':
         model = MCdropout_MLP(drop_prob=dropout,
                 opt='momentum', # TODO: try others?
                               n_inputs=grid_size**2,
@@ -170,15 +199,31 @@ if __name__ == "__main__":
                               n_units=hidden_size,
                               output_type='real')
         model.predict = model.predict_proba
-        # TODO: n=??
-        model.train_on_batch = lambda x,y: model.train_func(x,y,n=1000, lr=.2)
+        # FIXME: n=??
+        model.train_on_batch = lambda x,y: model.train_func(x,y,n=1000, lr=lr)
+    elif model == 'BHN':
+        model = MLPWeightNorm_BHN(lbda=10.,
+                opt='momentum',
+                                  n_inputs=grid_size**2,
+                                  n_classes=num_actions,
+                                  srng = RandomStreams(seed=seed),
+                                  coupling=0,
+                                  n_hiddens=2,
+                                  n_units=hidden_size,
+                                  flow='IAF',
+                                  output_type='real')
+        model.predict = model.predict_proba
+        # FIXME: n=??
+        model.train_on_batch = lambda x,y: model.train_func(x,y,n=1000, lr=lr)
+    else: 
+        assert False
 
 
-    # If you want to continue training from a previous model, just uncomment the line bellow
-    # model.load_weights("model.h5")
 
     # Define environment/game
-    env = Catch(grid_size)
+    # env = Catch(grid_size)
+
+    env = make_bootDQNChain()
 
     # Initialize experience replay object
     exp_replay = ExperienceReplay(max_memory=max_memory)
@@ -192,16 +237,44 @@ if __name__ == "__main__":
         # get initial input
         input_t = env.observe()
 
-        while not game_over:
-            input_tm1 = input_t
+        # TODO: TESTING    
+        if exploration == 'epsilon_greedy':
+            def policy(state):
+                if np.random.rand() <= epsilon:
+                    return  np.random.randint(0, num_actions)
+                else:
+                    return  env.mc_dropout_act(model, state, mc_samples=50) # TODO: hardcoded
+        elif exploration == 'RLSVI':
+            # sample a q-network for the entire episode
+            policy = lambda x: model.sample_qyx()(x).argmax()
+        elif exploration == 'TS':
+            # sample a q-network for each action
+            policy = lambda state: env.mc_dropout_act(model, state, mc_samples=1)
+        else:
+            assert False
+
+
+        ## eplen : timesteps in the environment (max t=15)
+        eplen = 15
+        for e in range(eplen):
+
+        # while not game_over:
+            input_tm1 = input_t.astype("float32")
             # get next action
-            if np.random.rand() <= epsilon:
-                action = np.random.randint(0, num_actions, size=1)
-            else:
-                action = env.mc_dropout_act(model, input_tm1)
+            action = policy(input_tm1)
+
+            import pdb; pdb.set_trace()
+
+            if 0:
+                if np.random.rand() <= epsilon:
+                    action = np.random.randint(0, num_actions, size=1)
+                else:
+                    action = env.mc_dropout_act(model, input_tm1)
 
             # apply action, get rewards and new state
             input_t, reward, game_over = env.act(action)
+            input_t = input_t.astype("float32")#, reward, game_over = env.act(action)
+             #= input_t.astype("float32")#, reward, game_over = env.act(action)
             if reward == 1:
                 win_cnt += 1
 
@@ -211,7 +284,7 @@ if __name__ == "__main__":
             # adapt model
             inputs, targets = exp_replay.get_batch(model, batch_size=batch_size)
 
-            loss += model.train_on_batch(inputs, targets)
+            loss += model.train_on_batch(inputs.astype("float32"), targets.astype("float32"))
         print("Epoch {:03d}/999 | Loss {:.4f} | Win count {}".format(e, loss, win_cnt))
 
     if 0:
@@ -219,3 +292,4 @@ if __name__ == "__main__":
         model.save_weights("model.h5", overwrite=True)
         with open("model.json", "w") as outfile:
             json.dump(model.to_json(), outfile)
+
