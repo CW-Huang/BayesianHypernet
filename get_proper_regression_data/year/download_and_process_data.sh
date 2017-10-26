8b1a5c1f596e0b97095019dbcc4b23ff36efc2bf
David Krueger <davidscottkrueger@gmail.com>
2017-10-26 01:42:11 -0400

    year data

--- get_proper_regression_data/year/download_and_process_data.sh
+++ get_proper_regression_data/year/download_and_process_data.sh
@@ -2,3 +2,7 @@ mkdir data
 cd data
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/00203/YearPredictionMSD.txt.zip
 unzip YearPredictionMSD.txt.zip
+cp split_data_train_test.py data
+mv data
+python split_data_train_test.py
+
