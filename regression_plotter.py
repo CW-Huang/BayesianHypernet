7b7ba9841780ebaac8044b20b3bf4cb246be6c41
David Krueger <davidscottkrueger@gmail.com>
2017-10-27 03:39:32 -0400

    regression_plotter.py WIP

--- regression_plotter.py
+++ regression_plotter.py
@@ -19,9 +19,24 @@ algorithm:
         for each split
 """
 
+vap = [p for p in os.listdir('.') if 'FINAL_va_LL' in p]
+var = {p: float(p.split('LL=')[1]) for p in vap}
+tep = [p for p in os.listdir('.') if 'FINAL_te_LL' in p]
+ter = {p: float(p.split('LL=')[1]) for p in tep}
 
+
+bests = {} # for all splits
+avg_bests = {} # averaged across splits
+
+# Here, we find the best hparams for each (dataset, model) INDEPENDENTLY for each split (to avoid hyperoptimization on test set)
+#   We average the test performances for each (dataset, model), using the hyperparameters that performed best on the valid set OF THAT SPLIT
+#   This gives us a fair estimate of the performance of the combined (model, hyperoptimization) algorithm.
+#       In our current case, hopt is just grid_search.
 for dat in dats:
     for model in models:
+        bests[(dat,model)] = {}
+        va_bests = [] 
+        te_bests = []
         for split in splits:
             # find the best results for this model on this dataset, FOR EACH SPLIT
             va_best = -np.inf
@@ -30,10 +45,19 @@ for dat in dats:
                 for lbda in lbda:
                     for lr in lrs:
                         ss = '/home/capybara/BayesianHypernetCW/launchers/launch_dev_regression.py/regression.py___dataset=' + dat + '_split=' + str(split)+ '_lr0=' + str(lr) + '_epochs=400_lbda=' + str(lbda) + '_model=' + model + '_tau=' + str(tau) + '_FINAL_va_LL='
-                        os.
-                        
-                k
-            best = launch_dev_regression.py/regression.py___dataset=parkinsons_split=18_lr0=.01_epochs=400_lbda=100.0_model=BHN_flow=IAF_coupling=4_tau=0.001_FINAL_va_LL=
+                        res = float([p for p in vap if p startswith ss][0].split('va_LL=')[1])
+                        if res > va_best:
+                            va_best = res
+                            va_best_ss = ss
+                            te_best = float([p for p in tep if p startswith ss][0].split('te_LL=')[1])
+            va_bests.append(va_best)
+            te_bests.append(te_best)
+            # save best
+            bests[(dat,model)][split] = (va_best, te_best, va_best_ss)
+            
+        avg_bests[(dat,model)] = (mean(va_bests), mean(te_bests))
+
+print avg_bests
 
 
 
@@ -42,22 +66,10 @@ for dat in dats:
 
 
 
+        # TODO: plotting (I'll want to load results above and average across splits... *sigh*
+
+    #figure(); suptitle('RMSE ' + dat)
+    #figure(); suptitle('LL ' + dat)
 
 
-
-
-        # TODO: plotting
-
-    figure(); suptitle('RMSE ' + dat)
-
-
-    
-
-
-
-
-    figure(); suptitle('LL ' + dat)
-
-
-    ss = '/home/capybara/BayesianHypernetCW/launchers/launch_dev_regression.py/regression.py___dataset=' + dat + '_split=' + str(split)+ '_lr0=' + str(lr) + '_epochs=400_lbda=' + str(lbda) + '_model=' + model
 
